5fd8c5163468c7b2f0a551f05e1441f84e6b70d8
k0bu <30616294+k0bu@users.noreply.github.com>
2018-12-30 15:44:20 +0900

    First time using paper.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
     'use strict';
-    console.log('jQueryはすでにロードされているはず。');
-    console.log('main.jsをロードした。');
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+
+    let c = Shape.Circle(200, 200, 50);
+    c.fillColor = 'green';
+
+    paper.view.draw();
 });
