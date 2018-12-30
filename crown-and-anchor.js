0206e230dd2b5d24996b7e79ef95eba484ca9249
k0bu <30616294+k0bu@users.noreply.github.com>
2018-12-31 00:26:41 +0900

    Completed implementation of the gamble game

--- crown-and-anchor.js
+++ crown-and-anchor.js
@@ -13,14 +13,14 @@ $(document).ready(function(){
     let funds = 50;
     let round = 0;
 
-    while(funds > 1 && funds < 100){
+    while(funds > 0 && funds < 100){
         round++;
 
         console.log(`${round} round:`);
         console.log(`Remaining fund: ${funds}`);
 
         let bets = {crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0};
-        let totalBet = rand(1, funnds);
+        let totalBet = rand(1, funds);
 
         if(totalBet === 7){
             totalBet = funds;
@@ -35,24 +35,33 @@ $(document).ready(function(){
             } while (remaining > 0);
         } 
         funds = funds - totalBet;
+
+        console.log(`Betting Money: ${totalBet}(`+
+            Object.keys(bets).map(face => `${face}: ${bets[face]}`).join(',') +
+        ")");
     
         const hand = [];
         for(let roll = 0; roll < 3; roll++){
             hand.push(randFace());
         }
 
+        console.log(`Die: ${hand.join(',')}`);
+
         let winnings = 0;
         for(let die = 0; die < hand.length; die++){
-            let face = hand[dice];
+            let face = hand[die];
             if(bets[face] > 0){
                 winnings = winnings + bets[face];
             }
         }
 
         funds += winnings;
-
+        console.log(`Winning money: ${winnings}`);
     }
 
     console.log(`Last remaining fund: ${funds}`);
+
+    if(funds > 100) console.log("Win");
+    else console.log("Lose");
 
 });
