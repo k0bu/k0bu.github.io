6c111b38a65007c225fbd9c4a5a0c35bb86bc23f
k0bu <30616294+k0bu@users.noreply.github.com>
2019-01-05 18:31:28 +0900

    ajax refactored with jquery

--- ajax-sample.js
+++ ajax-sample.js
@@ -1,21 +1,43 @@
-function refreshServerInfo() {
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', function() {
-    console.log(this.responseText);
+// function refreshServerInfo() {
+//   const req = new XMLHttpRequest();
+//   req.addEventListener('load', function() {
+//     console.log(this.responseText);
 
-    const data = JSON.parse(this.responseText);
-    console.log(Object.keys(data));
+//     const data = JSON.parse(this.responseText);
+//     console.log(Object.keys(data));
 
-    const serverInfo = document.querySelector('.serverInfo');
-    Object.keys(data).forEach(p => {
-      const replacements = serverInfo.querySelectorAll(`[data-replace="${p}"]`);
-      for(let r of replacements) {
-        r.textContent = data[p];
-      }
-    });
-  });
-  req.open('GET','http://localhost:7070', true);
-  req.send();
-};
+//     const serverInfo = document.querySelector('.serverInfo');
+//     Object.keys(data).forEach(p => {
+//       const replacements = serverInfo.querySelectorAll(`[data-replace="${p}"]`);
+//       for(let r of replacements) {
+//         r.textContent = data[p];
+//       }
+//     });
+//   });
+//   req.open('GET','http://localhost:7070', true);
+//   req.send();
+// };
 
 //setInterval(refreshServerInfo, 1000);
+
+$(document).ready(function(){
+  let TimerID = setInterval(refreshServerInfo, 200);
+
+  function refreshServerInfo(){
+    const $serverInfo = $('.serverInfo');
+    //jQuery.get.(~)
+    $.get('http://localhost:7070').then(
+      function (data) {
+        Object.keys(data).forEach( p=> {
+          $(`[data-replace="${p}"]`).text(data[p]);
+        });
+      },
+      function(jqXHR, textStatus,err){
+        const $errorInfo = $('.error');
+        console.error("Error has occured:" + err);
+        $errorINfo.text("Error: Cannot access the server.");
+        clearInterval(TimerID);
+      }
+    );
+  }
+});
--- ajax-server.js
+++ ajax-server.js
@@ -2,12 +2,14 @@ const http = require('http');
 
 const server =  http.createServer(function(req,res){
     res.setHeader('Content-Type', 'application.json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		
     res.end(JSON.stringify({
         platform: process.platform,
         nodeVersion: process.version,
         uptime: Math.round(process.uptime()),
-    }));
+		}));
+		
 });
 
 const port = 7070;
