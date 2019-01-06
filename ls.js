430c5e393e1c4cc11e4f2dc8d15bfe6a8d057974
k0bu <30616294+k0bu@users.noreply.github.com>
2019-01-06 17:52:40 +0900

    creating shell inside js and calling dir command

--- ls.js
+++ ls.js
@@ -5,3 +5,19 @@ fs.readdir(__dirname, function(err, files){
 	console.log(`List direcotry ${__dirname}:`);
 	console.log(files.map(f=> '\t' + f).join('\n'));
 });
+
+const exec = require('child_process').exec;
+
+const command = "dir";
+
+exec(command, function(err, stdout, stderr) {
+	if(err) return console.error(`Run time error ${command}`);
+	stdout = stdout.toString();
+	console.log(stdout);
+	stderr = stderr.toString();
+
+	if(stderr !== ''){
+		console.error('error:');
+		console.error(stderr);
+	}
+});
