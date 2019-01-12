77870f9a6da73d2d6073b75028bc9fc701237882
k0bu <30616294+k0bu@users.noreply.github.com>
2019-01-12 22:53:45 +0900

    minor change

--- server2.js
+++ server2.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer(function(req,res){
     if(req.method === 'GET' && req.url === '/favicon.ico'){
         const fs = require('fs');
-        const rs = fs.createReadStream('favicon.cio');
+        const rs = fs.createReadStream('favicon.ico');
         rs.pipe(res);
     } else {
         console.log(`${req.method} ${req.url}`);
